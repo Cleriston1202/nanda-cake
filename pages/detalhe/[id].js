bf8a6e4c0ab2309d4859312ae4670d311c8b7f31
Cleriston <Cleristonpnt5@gmail.com>
2021-05-29 16:27:59 -0300

    Update car left com quebra de linha

--- pages/detalhe/[id].js
+++ pages/detalhe/[id].js
@@ -32,16 +32,24 @@ export default function Detalhe() {
             </div>
         </div>
         <div className={styles.cardLeft}>
+                <h4 className={styles.cursoinfoh4}>Bolos deliciosos direto na sua casa Bolos deliciosos direto na sua casa</h4>
+                <p>{i.nome}</p>
+                <h4 className={styles.cursoinfoh4}>Bolos deliciosos direto na sua casa</h4>
+                <p>{i.nome}</p>
+                <h4 className={styles.cursoinfoh4}>Bolos deliciosos direto na sua casa</h4>
+                <p>{i.nome}</p>
+                <h4 className={styles.cursoinfoh4}>Bolos deliciosos direto na sua casa</h4>
+                <p>{i.nome}</p>
+                <h4 className={styles.cursoinfoh4}>Bolos deliciosos direto na sua casa</h4>
+                <p>{i.nome}</p>
+                <h4 className={styles.cursoinfoh4}>Bolos deliciosos direto na sua casa</h4>
+                <p>{i.nome}</p>
                 <h4 className={styles.cursoinfoh4}>Bolos deliciosos direto na sua casa</h4>
                 <p>{i.nome}</p>
                 <h4 className={styles.cursoinfoh4}>Bolos deliciosos direto na sua casa</h4>
                 <p>{i.nome}</p>
             </div>
-            <div className={styles.cursopreco}>
-                <span className={styles.precode}>R$80,00</span>
-                <span className={styles.precopor}>R$50,00</span>
-            
-            </div>
+        
          
      </div>
           ))}
